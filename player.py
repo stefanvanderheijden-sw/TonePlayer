7f1851c90f372cfaaa7452e2311c07994176c3f9
stefanvanderheijden <stefanvanderheijden@gmail.com>
2020-10-01 16:54:22 +0200

    add stuff

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 pygame.mixer.pre_init(176400, 16, 2, 4096)
 
 pygame.mixer.init()
-pygame.mixer.music.load("./sweep16.wav")
+pygame.mixer.music.load("./tone10.wav")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
     continue
